3264a08f2c895fe377530b3419ce8182c2a07f57
Packo <juansolar@unicauca.edu.co>
2021-01-18 12:54:19 -0500

    Editar precios

--- src/app/components/precio/precio.component.ts
+++ src/app/components/precio/precio.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Precio } from 'src/app/models/precio';
 import { MensajesService } from 'src/app/services/mensajes.service';
 
 @Component({
@@ -12,7 +13,9 @@ import { MensajesService } from 'src/app/services/mensajes.service';
 export class PrecioComponent implements OnInit {
 
   formGroup: FormGroup;
-  precios: Array<any> = new Array<any>()
+  precios: Array<Precio> = new Array<Precio>()
+  esEditar: boolean = false;
+  id: string = ""
   
   constructor(private fb: FormBuilder, private db: AngularFirestore, private mensajeService: MensajesService) { }
 
@@ -24,14 +27,21 @@ export class PrecioComponent implements OnInit {
       tipoDuracion: ['', Validators.required]
     })
 
-    this.db.collection("Precios").get().subscribe((data)=>{
+    this.mostrarPrecios();
+
+  }
+
+  mostrarPrecios(){
+    this.db.collection<Precio>("Precios").get().subscribe((data)=>{
+      this.precios.length = 0;
       data.docs.forEach((data) =>{
-        let precio = data.data();
+        let precio = data.data() as Precio;
         precio.id = data.id;
         precio.ref = data.ref;
         this.precios.push(precio);
       })
     })
+
   }
 
   agregar(){
@@ -39,8 +49,32 @@ export class PrecioComponent implements OnInit {
       () => { 
         this.mensajeService.mensajeCorrecto("Agregado", "Se agrego correctamente")
         this.formGroup.reset();
+        this.mostrarPrecios();
       }
     ).catch( () => {this.mensajeService.mensajeError("Error", "Ocurrio un error en agregar el precio")} )
+  }
+
+  editarPrecio(precio: Precio){
+    this.esEditar = true;
+    this.id = precio.id;
+    this.formGroup.setValue({
+      nombre: precio.nombre,
+      costo: precio.costo,
+      duracion: precio.duracion,
+      tipoDuracion: precio.tipoDuracion
+    })
+  }
+
+  editar(){
+    this.db.doc("Precios/" + this.id).update(this.formGroup.value).then(
+      () => { 
+        this.mensajeService.mensajeCorrecto("Actualizado","Precio actualizado correctamente");
+        this.formGroup.reset();
+        this.esEditar = false;
+        this.mostrarPrecios();
+    }).catch(
+      () => {this.mensajeService.mensajeError("Error", "Ocurrio un error en la actualización")}
+    )
   }
 
 }
