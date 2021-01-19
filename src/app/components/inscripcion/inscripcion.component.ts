86e9fabb5ae07854722289115139f1274c33f8aa
Packo <juansolar@unicauca.edu.co>
2021-01-19 11:57:45 -0500

    Selección de dias de inscripción

--- src/app/components/inscripcion/inscripcion.component.ts
+++ src/app/components/inscripcion/inscripcion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Cliente } from 'src/app/models/cliente';
 import { Inscripcion } from 'src/app/models/inscripcion';
+import { Precio } from 'src/app/models/precio';
 
 @Component({
   selector: 'app-inscripcion',
@@ -11,10 +13,21 @@ export class InscripcionComponent implements OnInit {
 
   inscripcion: Inscripcion = new Inscripcion();
   cliente: Cliente = new Cliente();
+  precios: Precio[] = new Array<Precio>();
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.db.collection<Precio>('Precios').get().subscribe(
+    (data) =>{
+      data.docs.forEach((item) =>{
+        let precio = item.data();
+        precio.id = item.id;
+        precio.ref = precio.ref;
+        this.precios.push(precio);
+      })
+    }
+    )
   }
 
   asingarCliente(cliente: Cliente){
