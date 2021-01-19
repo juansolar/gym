10424e7ec2804025a180fa0543152c5dc70d8b41
Packo <juansolar@unicauca.edu.co>
2021-01-19 14:04:35 -0500

    Validación de datos de inscripción

--- src/app/components/inscripcion/inscripcion.component.ts
+++ src/app/components/inscripcion/inscripcion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { ɵangular_packages_router_router_n } from '@angular/router';
 import { Cliente } from 'src/app/models/cliente';
 import { Inscripcion } from 'src/app/models/inscripcion';
 import { Precio } from 'src/app/models/precio';
@@ -43,56 +44,70 @@ export class InscripcionComponent implements OnInit {
   }
 
   guardar(){
-    console.log(this.inscripcion);
+    if(this.inscripcion.validar().esValido){
+      console.log("Guardando")
+    }else
+      console.log(this.inscripcion.validar().mensaje)
   }
 
   seleccionarPrecio(id: string){
-    this.precioSeleccionado = this.precios.find(x => x.id == id)
-    this.inscripcion.precio = this.precioSeleccionado.ref;
 
-    this.inscripcion.fecha = new Date();
+    if(id == "null"){
+      this.precioSeleccionado = new Precio();
+      this.inscripcion.precio = null;
+      this.inscripcion.subTotal = 0;
+      this.inscripcion.iva = 0;
+      this.inscripcion.total = 0;
+      this.inscripcion.fecha = null;
+      this.inscripcion.fechaFinal = null;
+      
+    }else{
+      this.precioSeleccionado = this.precios.find(x => x.id == id)
+      this.inscripcion.precio = this.precioSeleccionado.ref;
 
-    let anio = this.inscripcion.fecha.getFullYear();
-    let mes = this.inscripcion.fecha.getMonth();
-    let dia = this.inscripcion.fecha.getDate();
+      this.inscripcion.fecha = new Date();
 
-    if(this.precioSeleccionado.tipoDuracion == 1){
-      //dia
-      let dias: number = this.precioSeleccionado.duracion;
-      let fechaFinal = new Date(anio,mes,dia + dias)
-      this.inscripcion.fechaFinal = fechaFinal;
-    }
-    if(this.precioSeleccionado.tipoDuracion == 2){
-      //semana
-      let dias: number = this.precioSeleccionado.duracion * 7;
-      let fechaFinal = new Date(anio,mes,dia + dias)
-      this.inscripcion.fechaFinal = fechaFinal;
-    }
-    if(this.precioSeleccionado.tipoDuracion == 3){
-      //quincena
-      let dias: number = this.precioSeleccionado.duracion * 15;
-      let fechaFinal = new Date(anio,mes,dia + dias)
-      this.inscripcion.fechaFinal = fechaFinal;
-    }
-    if(this.precioSeleccionado.tipoDuracion == 4){
-      //mes
-      let meses: number = this.precioSeleccionado.duracion + mes;
-      let fechaFinal = new Date(anio, meses,dia)
-      this.inscripcion.fechaFinal = fechaFinal;
-    }
-    if(this.precioSeleccionado.tipoDuracion == 5){
-      //año
-      let anios: number = this.precioSeleccionado.duracion + anio;
-      let fechaFinal = new Date(anios,mes,dia)
-      this.inscripcion.fechaFinal = fechaFinal;
-    }
+      let anio = this.inscripcion.fecha.getFullYear();
+      let mes = this.inscripcion.fecha.getMonth();
+      let dia = this.inscripcion.fecha.getDate();
 
-    this.inscripcion.subTotal = this.precioSeleccionado.costo;
-    this.inscripcion.iva = this.inscripcion.subTotal * 0.19;
-    this.inscripcion.total = this.inscripcion.iva + this.inscripcion.subTotal;
+      if(this.precioSeleccionado.tipoDuracion == 1){
+        //dia
+        let dias: number = this.precioSeleccionado.duracion;
+        let fechaFinal = new Date(anio,mes,dia + dias)
+        this.inscripcion.fechaFinal = fechaFinal;
+      }
+      if(this.precioSeleccionado.tipoDuracion == 2){
+        //semana
+        let dias: number = this.precioSeleccionado.duracion * 7;
+        let fechaFinal = new Date(anio,mes,dia + dias)
+        this.inscripcion.fechaFinal = fechaFinal;
+      }
+      if(this.precioSeleccionado.tipoDuracion == 3){
+        //quincena
+        let dias: number = this.precioSeleccionado.duracion * 15;
+        let fechaFinal = new Date(anio,mes,dia + dias)
+        this.inscripcion.fechaFinal = fechaFinal;
+      }
+      if(this.precioSeleccionado.tipoDuracion == 4){
+        //mes
+        let meses: number = this.precioSeleccionado.duracion + mes;
+        let fechaFinal = new Date(anio, meses,dia)
+        this.inscripcion.fechaFinal = fechaFinal;
+      }
+      if(this.precioSeleccionado.tipoDuracion == 5){
+        //año
+        let anios: number = this.precioSeleccionado.duracion + anio;
+        let fechaFinal = new Date(anios,mes,dia)
+        this.inscripcion.fechaFinal = fechaFinal;
+      }
 
+      this.inscripcion.subTotal = this.precioSeleccionado.costo;
+      this.inscripcion.iva = this.inscripcion.subTotal * 0.19;
+      this.inscripcion.total = this.inscripcion.iva + this.inscripcion.subTotal;
+
+    }
   }
-
 
 }
 
--- src/app/models/inscripcion.ts
+++ src/app/models/inscripcion.ts
@@ -17,6 +17,50 @@ export class Inscripcion{
         this.subTotal = this.subTotal;
         this.iva = this.iva;
         this.total = this.total
-
     }
+
+    validar(): any{
+        let rta = {
+            esValido: false,
+            mensaje: ""
+        }
+        if(this.cliente == null || this.cliente == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No ha seleccionado un cliente"
+            return rta;
+        }
+        if(this.precio == null || this.precio == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No ha seleccionado un precio"
+            return rta;
+        }
+        if(this.fecha == null || this.fecha == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No tiene fecha de inicio"
+            return rta;
+        }
+        if(this.fechaFinal == null || this.fechaFinal == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No tiene fecha final"
+            return rta;
+        }
+        if(this.subTotal <= 0 || this.subTotal == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No se ha podido calcular el subtotal"
+            return rta;
+        }
+        if(this.iva <= 0 || this.iva == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No se ha podido calcular el IVA"
+            return rta;
+        }
+        if(this.total <= 0 || this.total == undefined){
+            rta.esValido = false;
+            rta.mensaje = "No se ha podido calcular el Total"
+            return rta;
+        }
+        rta.esValido = true
+        return rta;
+    }
+
 }
